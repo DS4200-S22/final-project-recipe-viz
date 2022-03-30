5e533872bb8b69e8001678d58f76ef46b6fec05f
Julie Wang <juliewang@Julies-MacBook-Pro.local>
2022-03-29 23:40:33 -0400

    added functioning wordcloud

--- js/wordcloud.js
+++ js/wordcloud.js
@@ -1,12 +1,48 @@
-const margin = {
-  top: 80,
-  right: 80,
-  bottom: 20,
-  left: 20
-};
-const width = 400 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+// const margin = {
+//   top: 80,
+//   right: 80,
+//   bottom: 20,
+//   left: 20
+// };
+// const width = 400 - margin.left - margin.right;
+// const height = 400 - margin.top - margin.bottom;
 
-var svg = d3.select("#vis-container").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
+// List of words
+var myWords = [{word: "Banana", size: "10"}, {word: "Bread", size: "20"}, {word: "Egg", size: "50"}, {word: "Flour", size: "40"}, {word: "Chocolate", size: "20"}, {word: "Salt", size: "60"} ]
+
+// append the svg object to the body of the page
+var svg = d3.select("#word-cloud").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+// Wordcloud features that are different from one word to the other must be here
+var layout = d3.layout.cloud()
+  .size([width, height])
+  .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+  .padding(5)        //space between words
+  .rotate(function() { return ~~(Math.random() * 2) * 90; })
+  .fontSize(function(d) { return d.size; })      // font size of words
+  .on("end", draw);
+layout.start();
+
+// This function takes the output of 'layout' above and draw the words
+// Wordcloud features that are THE SAME from one word to the other can be here
+function draw(words) {
+  svg
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("fill", "#69b3a2")
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+}
