a74e18bcde94ee8e0e2f44c34259ef16adcccfeb
fzyang1227 <62119227+fzyang1227@users.noreply.github.com>
2022-04-07 00:01:13 -0400

    connected filters to scatterplot

--- js/visualization.js
+++ js/visualization.js
@@ -5,8 +5,25 @@ const margin = {
     bottom: 20,
     left: 20
   };
-const width = 400 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+
+const width = 900 - margin.left - margin.right;
+const height = 900 - margin.top - margin.bottom;
+
+const svg = d3.select("#vis-container")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          `translate(${margin.left}, ${margin.top})`);
+
+let dot
+
+// Scales are global
+let x, y
+
+// Keys are global
+let xKey1, yKey1
 
 // Filtering Axes
 d3.csv("data/recipe_tot.csv").then(function(data) {
@@ -14,10 +31,47 @@ d3.csv("data/recipe_tot.csv").then(function(data) {
                       'sugar (g)','sodium (mg)','protein (g)','saturated fat (g)','carbohydrates (g)'];
 
 
+    xKey1 = 'minutes'
+    yKey1 = 'minutes'
+
+    let maxX = d3.max(data, (d) => { return d[xKey1]; });
+
+    // Add X axis
+    x = d3.scaleLinear()
+      .domain([0, maxX])
+      .range([margin.left, width-margin.right]);
+
+    let xAxis = svg.append("g")
+                      .attr("transform", `translate(0, ${height - margin.bottom})`)
+                      .call(d3.axisBottom(x));
+
+    // Find max y 
+    let maxY = d3.max(data, (d) => { return d[yKey1]; });
+
+    // Add Y axis
+    y = d3.scaleLinear()
+      .domain([0, maxY])
+      .range([height - margin.bottom, margin.top]);
+
+    let yAxis = svg.append("g")
+                      .attr("transform", `translate(${margin.left}, 0)`) 
+                      .call(d3.axisLeft(y)); 
+
     // add the options to the button
     let dropdownY = d3.select("#visbutton")
       .append('select')
       .attr('id', 'dropY');
+
+    // Add points
+    dot = svg.selectAll("circle")
+                        .data(data)
+                        .enter()
+                          .append("circle")
+                          .attr("id", (d) => d.id)
+                          .attr("cx", (d) => x(d[xKey1]))
+                          .attr("cy", (d) => y(d[yKey1]))
+                          .attr("r", 5)
+                          .style("opacity", 0.5);
 
     // add the options to the button
     dropdownY // Add a button
@@ -49,6 +103,11 @@ d3.csv("data/recipe_tot.csv").then(function(data) {
       // Create new data with the selection?
       const dataFilterX = data.map(function(d){return {valueX:d[selectedGroup]} })
 
+      // Update X axis
+      maxX = d3.max(data, (d) => { return d[selectedGroup]; });
+      x.domain([0,maxX])
+      xAxis.transition().duration(1000).call(d3.axisBottom(x))
+
       // Give these new data to update dot
       dot
         .data(dataFilterX)
@@ -63,6 +122,11 @@ d3.csv("data/recipe_tot.csv").then(function(data) {
       // Create new data with the selection?
       const dataFilterY = data.map(function(d){return {valueY:d[selectedGroup]} })
 
+      // Update Y axis
+      maxY = d3.max(data, (d) => { return d[selectedGroup]; });
+      y.domain([0,maxY])
+      yAxis.transition().duration(1000).call(d3.axisLeft(y))
+
       // Give these new data to update dot
       dot
         .data(dataFilterY)
@@ -73,51 +137,22 @@ d3.csv("data/recipe_tot.csv").then(function(data) {
 
     // When the button is changed, run the updateChart function
     d3.select("#dropX").on("change", function(event, d) {
+
         // recover the option that has been chosen
         let selectedOption = d3.select(this).property("value")
+
         // run the updateChart function with this selected option
         updateX(selectedOption)
     })
 
     // When the button is changed, run the updateChart function
     d3.select("#dropY").on("change", function(event, d) {
+
         // recover the option that has been chosen
         let selectedOption = d3.select(this).property("value")
+
         // run the updateChart function with this selected option
         updateY(selectedOption)
+
+    })
 });
-
-const svg1 = d3.select("#vis-container").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-
-const g = svg1.append("g")
-  .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
-
-const x = d3.scaleLinear().range([0, width]).domain([0, 10]);
-const y = d3.scaleLinear().range([height, 0]).domain([0, 10]);
-
-// Add the X Axis
-g.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
-
-// Add the Y Axis
-g.append("g")
-  .call(d3.axisLeft(y));
-
-
-g.selectAll(".point")
-  .data(data)
-  .enter()
-  .append("circle")
-  .attr("cx", function(d) {
-    return x(d[0]);
-  })
-  .attr("cy", function(d) {
-    return y(d[1]);
-  })
-  .attr("r", 7)
-  .style("fill", "steelblue")
-  .style("stroke", "lightgray");
