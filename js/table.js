10675f1c3773d6a48b1ce45aa254f666639612e7
fzyang1227 <62119227+fzyang1227@users.noreply.github.com>
2022-04-14 01:51:36 -0400

    table added

---
+++ js/table.js
@@ -0,0 +1,65 @@
+d3.csv("../data/recipe_tot2.csv").then((data) => {
+
+    // Table
+    // the columns you'd like to display
+    let columns = [
+        "name", "minutes","calories (kCal)","total fat (g)",
+        "sugar (g)","sodium (mg)","protein (g)","saturated fat (g)",
+        "carbohydrates (g)","rating",
+    ];
+
+    data.forEach(function(d){
+    for (let i = 0; i < columns.length; i++) {
+        if (columns[i] === "name") { continue; } 
+        d[columns[i]] = +d[columns[i]]; };
+    });
+
+    let table = d3.select("#recipe-table").append("table"),
+    thead = table.append("thead"),
+    tbody = table.append("tbody");
+
+    // append the header row
+    thead.append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+        .text(function (column) {
+          return column;
+        })
+
+    // create a row for each object in the data
+    function addData(data) {
+        rows = tbody.selectAll("tr")
+                  .data(data)
+                  .enter()
+                  .append("tr")
+                    .on("click", function(event, d) {
+                          recipeUrl = "https://www.food.com/recipe/-" + d.id
+                          window.open(recipeUrl)
+                      });
+
+        // create a cell in each row for each column
+        cells = rows.selectAll("td")
+                  .data(function (row) {
+                    return columns.map(function (column) {
+                      return { column: column, value: row[column] };
+                    });
+                  })
+                  .enter()
+                  .append("td")
+                  .text(function (d) {
+                    return d.value;
+                  });
+    }
+
+    function sortTable(col) {
+        table.selectAll("tbody tr") 
+            .sort(function(a, b) {
+                    return d3.descending(a[col], b[col]);
+            });
+        }
+
+    addData(data);
+    sortTable("minutes")
+});
--- js/visualization.js
+++ js/visualization.js
@@ -1,13 +1,15 @@
 // setting parameters
 const margin = {
-    top: 20,
-    right: 20,
+    top: 80,
+    right: 80,
     bottom: 20,
     left: 20
   };
 
 const width = 700 - margin.left - margin.right;
-const height = 700 - margin.top - margin.bottom;
+let height = 700 - margin.top - margin.bottom;
+
+const ingredientsObj = {}
 
 const svg = d3.select("#vis-container")
   .append("svg")
@@ -18,6 +20,7 @@ const svg = d3.select("#vis-container")
           `translate(${margin.left}, ${margin.top})`);
 
 let dot
+let title
 
 // Scales are global
 let x, y
@@ -25,44 +28,102 @@ let x, y
 // Keys are global
 let xKey1, yKey1
 
-
 // Filtering Axes
 d3.csv("data/recipe_tot2.csv").then(function(data) {
+
+    // WORDCLOUD
+    let clickedIngredients = [];
+    let myWords = wordCloud(data);
+
+    let height = 500 - margin.top - margin.bottom;
+
+      // append the svg object to the body of the page
+    const svg2 = d3.select("#word-cloud").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+    const mouseoverWords = function(d, i) {
+      d3.select(this).style("cursor", "pointer"); 
+      if(d3.select(this).style('fill') != 'rgb(252, 132, 3)') {
+        d3.select(this).style('fill', 'orange');
+      }
+    }
+
+    const mouseoutWords = function(d, i) {
+      if(d3.select(this).style('fill') != 'rgb(252, 132, 3)') {
+        d3.select(this).style('fill', '#69b3a2');
+      }
+    }
+
+    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+    // Wordcloud features that are different from one word to the other must be here
+    const layout = d3.layout.cloud()
+    .size([width, height])
+    .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+    .padding(5)        //space between words
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .fontSize(function(d) { return 2.5*Math.sqrt(d.size); })      // font size of words
+    .on("end", draw);
+    layout.start();
+
+    // This function takes the output of 'layout' above and draw the words
+    // Wordcloud features that are THE SAME from one word to the other can be here
+    function draw(words) {
+      myWords = svg2
+        .append("g")
+          .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+          .selectAll("text")
+          .attr("id", "wordsClass")
+            .data(words)
+          .enter().append("text")
+            .style("font-size", function(d) { 
+              return d.size + "px"; 
+            })
+            .style("fill", "#69b3a2")
+            .attr("text-anchor", "middle")
+            .attr("transform", function(d) {
+              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; })
+            .on("click", function(d, i) {
+              if(d3.select(this).style('fill') == 'rgb(252, 132, 3)') {
+                d3.select(this).style('fill', '#69b3a2');
+                clickedIngredients.splice(clickedIngredients.indexOf(i.text), 1);
+                d3.selectAll('circle').style("r", (d) => {
+                  if(clickedIngredients.every((ingredient) => d.ingredients.includes(ingredient)) || clickedIngredients.length == 0) {
+                    return "5px";
+                  } else {
+                    return "0px";
+                  }
+                })
+              } else {
+                clickedIngredients.push(i.text)
+                d3.select(this).style('fill', '#fc8403');
+                d3.selectAll('circle').style("r", (d) => {
+                  if(clickedIngredients.every((ingredient) => d.ingredients.includes(ingredient)) || clickedIngredients.length == 0) {
+                    return "5px";
+                  } else {
+                    return "0px";
+                  }
+                })
+              }
+            })
+            .on("mouseover", mouseoverWords)
+            .on("mouseout", mouseoutWords)
+    }
+
+    height = 700 - margin.top - margin.bottom;
+
+
+    // SCATTERPLOT
     const recipeAttr = ['minutes','n_steps','n_ingredients','calories (kCal)','total fat (g)',
                       'sugar (g)','sodium (mg)','protein (g)','saturated fat (g)','carbohydrates (g)'];
 
-
     xKey1 = 'minutes'
-    yKey1 = 'n_steps'
-
-    // add the options to the button
-    let dropdownY = d3.select("#vis-container")
-      .append('select')
-      .attr('id', 'dropY');
-      
-    // add the options to the button
-    dropdownY // Add a button
-      .selectAll('myOptions') // Next 4 lines add 6 options = 6 colors
-        .data(recipeAttr)
-      .enter()
-        .append('option')
-      .text(function (d) { return d; }) // text showed in the menu
-      .attr("value", function (d) { return d; }) // corresponding value returned by the button
-
-    
-    // add the options to the button
-    let dropdownX = d3.select("#vis-container")
-      .append('select')
-      .attr('id', 'dropX');
-
-    // add the options to the button
-    dropdownX // Add a button
-      .selectAll('myOptions') // Next 4 lines add 6 options = 6 colors
-        .data(recipeAttr)
-      .enter()
-        .append('option')
-      .text(function (d) { return d; }) // text showed in the menu
-      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+    yKey1 = 'minutes'
 
     let maxX = d3.max(data, (d) => { return parseInt(d[xKey1]); });
 
@@ -77,6 +138,7 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
 
     // Find max y 
     let maxY = d3.max(data, (d) => { return parseInt(d[yKey1]); });
+    console.log(maxY);
 
     // Add Y axis
     y = d3.scaleLinear()
@@ -87,6 +149,11 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
                       .attr("transform", `translate(${margin.left}, 0)`) 
                       .call(d3.axisLeft(y));
 
+
+    // add the options to the button
+    let dropdownY = d3.select("#axisY")
+      .append('select')
+      .attr('id', 'dropY')
 
     // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
     // Its opacity is set to 0: we don't see it by default.
@@ -108,6 +175,7 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
       tooltip
         .html(`Name: ${d['name']}<br>ID: ${d['id']}<br>${xKey1}: ${d[xKey1]}<br>${yKey1}: ${d[yKey1]}`)
         .style("opacity", 1)
+      d3.select(this).style("cursor", "pointer");
     }
 
     const mousemove = function(event, d) {
@@ -129,21 +197,112 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
       window.open(recipeUrl)
     }
 
+    const dotColors = {"breakfast": "#ff87ab", 
+                      "lunch": "#52b788",
+                      "dinner": "#1e6091"}
+
+    const dotMeal = Object.keys(dotColors);
+    const mealColor = Object.values(dotColors);
+
     // Add points
     dot = svg.selectAll("circle")
                         .data(data)
                         .enter()
                           .append("circle")
-                          .attr("id", (d) => d.id)
+                          .attr("id", "circleClass")
                           .attr("cx", (d) => x(d[xKey1]))
                           .attr("cy", (d) => y(d[yKey1]))
-                          .attr("r", 3)
+                          .attr("r", 5)
+                          .style("fill", (d => dotColors[d["meal"]]))
                           .style("opacity", 0.5)
                         .on("mouseover", mouseover )
                         .on("mousemove", mousemove )
                         .on("mouseleave", mouseleave )
-                        .on('click', mouseclick )
+                        .on("click", mouseclick );
 
+    svg.selectAll("legendSymbols")
+        .data(dotMeal)
+        .enter()
+        .append("circle")
+        .attr("cx", width - margin.right - 200)
+        .attr("cy", (d, i) => 150 + i * 25)
+        .attr("r", 5)
+        .attr("fill", (d, i) => mealColor[i])
+
+     svg.selectAll("legendTexts")
+        .data(dotMeal)
+        .enter()
+        .append("text")
+        .text((d) => d)
+        .attr("x", width - margin.right - 200 + 15)
+        .attr("y", (d, i) => 150 + i * 25 + 5)
+        .attr("class", "textbox")              
+
+    // add the options to the button
+    dropdownY // Add a button
+      .selectAll('myOptions') // Next 4 lines add 6 options = 6 colors
+        .data(recipeAttr)
+      .enter()
+        .append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+    
+    // add the options to the button
+    // let dropdownX = d3.select("#visbuttonX")
+    let dropdownX = d3.select("#axisX")
+      .append('select')
+      .attr('id', 'dropX');
+
+    // add the options to the button
+    dropdownX // Add a button
+      .selectAll('myOptions') // Next 4 lines add 6 options = 6 colors
+        .data(recipeAttr)
+      .enter()
+        .append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+    const selectedTextX = d3.select('#axisX option:checked').text();
+    const selectedTextY = d3.select('#axisY option:checked').text();
+
+    title = svg.append("text")
+      .attr("id", "title-text")
+      .attr("x", (width / 2))             
+      .attr("y", 0 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "16px")
+      .text(`${selectedTextY} vs. ${selectedTextX}`)
+
+    function updateTitleX(newAxisTitle) {
+
+      const selectedTextY = d3.select('#axisY option:checked').text();
+      
+      d3.select('#title-text').remove()
+  
+      title = svg.append("text")
+        .attr("id", "title-text")
+        .attr("x", (width / 2))             
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px")
+        .text(`${selectedTextY} vs. ${newAxisTitle}`)
+    }
+
+    function updateTitleY(newAxisTitle) {
+
+      const selectedTextX = d3.select('#axisX option:checked').text();
+
+      d3.select('#title-text').remove()
+  
+      title = svg.append("text")
+        .attr("id", "title-text")
+        .attr("x", (width / 2))             
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px")
+        .text(`${newAxisTitle} vs. ${selectedTextX}`)
+    }
 
     // A function that update the chart
     function updateX(selectedGroup) {
@@ -168,10 +327,11 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
     function updateY(selectedGroup) {
 
       yKey1 = selectedGroup
-
+      
       // Update Y axis
       maxY = d3.max(data, (d) => { return parseInt(d[selectedGroup]); });
       y.domain([0,maxY])
+      console.log(maxY);
       yAxis.transition().duration(1000).call(d3.axisLeft(y))
 
       // Give these new data to update dot
@@ -181,7 +341,7 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
         .duration(1000)
           .attr("cy", d => y(+d[selectedGroup]))
     };
-
+    
     // When the button is changed, run the updateChart function
     d3.select("#dropX").on("change", function(event, d) {
 
@@ -190,6 +350,8 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
 
         // run the updateChart function with this selected option
         updateX(selectedOption)
+
+        updateTitleX(selectedOption)
     })
 
     // When the button is changed, run the updateChart function
@@ -201,101 +363,60 @@ d3.csv("data/recipe_tot2.csv").then(function(data) {
         // run the updateChart function with this selected option
         updateY(selectedOption)
 
+        updateTitleY(selectedOption)
+
     })
-    /*
-    // top histogram
-    const gTop = svg.append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-        // "translate(0,0)");
-
-    const xBins = d3.histogram()
-      .domain(x.domain())
-      .thresholds(x.ticks(10))
-      .value(function(d) {
-        return d[xKey1];
-      })(data);
-
-    const xy = d3.scaleLinear()
-      .domain([0, d3.max(xBins, function(d) {
-        return d.length;
-      })])
-      .range([margin.top, 0]);
-
-    const xBar = gTop.selectAll(".bar")
-      .data(xBins)
-      .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d) {
-        return "translate(" + x(d.x0) + "," + xy(d.length) + ")";
-      });
-
-    let bWidth = x(xBins[0].x1) - x(xBins[0].x0) - 1;
-    xBar.append("rect")
-      .attr("x", 1)
-      .attr("width", bWidth)
-      .attr("height", function(d) {
-        return margin.top - xy(d.length);
-      })
-      .style("fill", "steelblue");
-
-    xBar.append("text")
-      .attr("dy", "-0.25em")
-      .attr("y", 2)
-      .attr("x", bWidth / 2)
-      .attr("text-anchor", "middle")
-      .text(function(d) {
-        return d.length < 4 ? "" : d.length;
-      })
-      .style("fill", "black")
-      .style("font", "9px sans-serif");
-      
-    // right histogram
-    const gRight = svg.append("g")
-      .attr("transform",
-        "translate(" + (margin.left + width) + "," + margin.top + ")");
-
-    const yBins = d3.histogram()
-      .domain(y.domain())
-      .thresholds(y.ticks(10))
-      .value(function(d) {
-        return d[1];
-      })(data);
-
-    const yx = d3.scaleLinear()
-      .domain([0, d3.max(yBins, function(d) {
-        return d.length;
-      })])
-      .range([0, margin.right]);
-
-    const yBar = gRight.selectAll(".bar")
-      .data(yBins)
-      .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d) {
-        return "translate(" + 0 + "," + y(d.x1) + ")";
-      });
-
-    bWidth = y(yBins[0].x0) - y(yBins[0].x1) - 1;
-    yBar.append("rect")
-      .attr("y", 1)
-      .attr("width", function(d){
-        return yx(d.length);
-      })
-      .attr("height", bWidth)
-      .style("fill", "steelblue");
-
-    yBar.append("text")
-      .attr("dx", "-.75em")
-      .attr("y", bWidth / 2 + 1)
-      .attr("x", function(d){
-        return yx(d.length);
-      })
-      .attr("text-anchor", "middle")
-      .text(function(d) {
-        return d.length < 4 ? "" : d.length;
-      })
-      .style("fill", "white")
-      .style("font", "9px sans-serif");
-      */
 });
+
+
+function wordCloud(data) {
+  for (let i = 0; i < data.length; i++) {
+    const ingredientsString = data[i]['ingredients']
+    const cleanedIngredients = ingredientsString.substring(1, ingredientsString.length - 1).split(', ').map((item) => item.substring(1, item.length - 1))
+    
+    cleanedIngredients.forEach((ingredient) => {
+      if(Object.keys(ingredientsObj).includes(ingredient)) {
+        ingredientsObj[ingredient] = ingredientsObj[ingredient] + 1;
+      } else {
+        ingredientsObj[ingredient] = 1;
+      }
+    })
+  }
+
+  Object.keys(ingredientsObj).forEach((ingredient) => {
+    if(ingredientsObj[ingredient] < 10) {
+      delete ingredientsObj[ingredient];
+    }
+  })
+
+  delete ingredientsObj['salt'];
+  delete ingredientsObj['water'];
+  delete ingredientsObj['butter'];
+  delete ingredientsObj['olive oil'];
+  delete ingredientsObj['pepper'];
+
+  ingredientsObj['garlic cloves'] = ingredientsObj['garlic cloves'] + ingredientsObj['garlic']
+  delete ingredientsObj['garlic']
+
+  ingredientsObj['egg'] = ingredientsObj['egg'] + ingredientsObj['eggs']
+  delete ingredientsObj['eggs']
+
+
+  let myWords = []
+  Object.keys(ingredientsObj).forEach((ingredient) => {
+    const wordsObject = {word: ingredient, size: ingredientsObj[ingredient]};
+    myWords.push(wordsObject);
+  })
+
+  myWords.sort((a, b) => a.size < b.size);
+
+  return myWords;
+}
+
+function clearAll() {
+  console.log('hello')
+  // console.log(d3.selectAll('text').style("fill"))
+  d3.selectAll('text').style("fill", "#69b3a2")
+  d3.selectAll('circle').style('r', '5px')
+  clickedIngredients = []
+}
